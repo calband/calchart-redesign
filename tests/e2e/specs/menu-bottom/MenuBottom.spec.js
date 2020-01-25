f6865554a80b7fabe2955cbae51a844b20ffa74a
Michael Tamaki <themichaeltamaki@gmail.com>
2020-01-25 12:11:04 -0800

    Fix e2e menu bottom tests on ci

--- tests/e2e/specs/menu-bottom/MenuBottom.spec.js
+++ tests/e2e/specs/menu-bottom/MenuBottom.spec.js
@@ -7,23 +7,20 @@ describe('components/menu-bottom/MenuBottom', () => {
     cy.get('[data-test="menu-bottom--tool-button"]')
       .should('have.length', 2);
 
-    cy.get('[data-test="menu-bottom--tool-button"].is-primary')
-      .should('have.length', 1)
-      .find('.mdi-hand-right')
-      .should('exist');
+    cy.get('[data-test="menu-bottom--tool-button"]')
+      .eq(0)
+      .should('have.class', 'is-primary');
 
     cy.get('[data-test="menu-bottom--tool-button"].is-light')
       .should('have.length', 1);
   });
 
   it('clicking on add/remove single dot disables pan/zoom', () => {
-    cy.get('[data-test="menu-bottom--tool-button"] .mdi-plus-minus')
-      .click();
-
-    cy.get('[data-test="menu-bottom--tool-button"].is-primary')
-      .should('have.length', 1)
-      .find('.mdi-plus-minus')
-      .should('exist');
+    cy.get('[data-test="menu-bottom--tool-button"]')
+      .eq(1)
+      .should('not.have.class', 'is-primary')
+      .click()
+      .should('have.class', 'is-primary');
 
     cy.get('[data-test="menu-bottom--tool-button"].is-light')
       .should('have.length', 1);
@@ -33,16 +30,15 @@ describe('components/menu-bottom/MenuBottom', () => {
   });
 
   it('clicking from and to pan/zoom enables pan/zoom', () => {
-    cy.get('[data-test="menu-bottom--tool-button"] .mdi-plus-minus')
+    cy.get('[data-test="menu-bottom--tool-button"]')
+      .eq(1)
       .click();
 
-    cy.get('[data-test="menu-bottom--tool-button"] .mdi-hand-right')
-      .click();
-
-    cy.get('[data-test="menu-bottom--tool-button"].is-primary')
-      .should('have.length', 1)
-      .find('.mdi-hand-right')
-      .should('exist');
+    cy.get('[data-test="menu-bottom--tool-button"]')
+      .eq(0)
+      .should('not.have.class', 'is-primary')
+      .click()
+      .should('have.class', 'is-primary');
 
     cy.get('#svg-pan-zoom-controls')
       .should('be.visible');
@@ -50,13 +46,10 @@ describe('components/menu-bottom/MenuBottom', () => {
 
   it('control key enables pan/zoom', () => {
     // Starting tool is add/remove single dot
-    cy.get('[data-test="menu-bottom--tool-button"] .mdi-plus-minus')
-      .click();
-
-    cy.get('[data-test="menu-bottom--tool-button"].is-primary')
-      .should('have.length', 1)
-      .find('.mdi-plus-minus')
-      .should('exist');
+    cy.get('[data-test="menu-bottom--tool-button"]')
+      .eq(1)
+      .click()
+      .should('have.class', 'is-primary');
 
     cy.get('#svg-pan-zoom-controls')
       .should('not.be.visible');
@@ -65,10 +58,9 @@ describe('components/menu-bottom/MenuBottom', () => {
     cy.document()
       .trigger('keydown', { key: 'Control' });
 
-    cy.get('[data-test="menu-bottom--tool-button"].is-primary')
-      .should('have.length', 1)
-      .find('.mdi-hand-right')
-      .should('exist');
+    cy.get('[data-test="menu-bottom--tool-button"]')
+      .eq(0)
+      .should('have.class', 'is-primary');
 
     cy.get('#svg-pan-zoom-controls')
       .should('be.visible');
@@ -77,10 +69,9 @@ describe('components/menu-bottom/MenuBottom', () => {
     cy.document()
       .trigger('keyup', { key: 'Control' });
 
-    cy.get('[data-test="menu-bottom--tool-button"].is-primary')
-      .should('have.length', 1)
-      .find('.mdi-plus-minus')
-      .should('exist');
+    cy.get('[data-test="menu-bottom--tool-button"]')
+      .eq(1)
+      .should('have.class', 'is-primary');
 
     cy.get('#svg-pan-zoom-controls')
       .should('not.be.visible');
