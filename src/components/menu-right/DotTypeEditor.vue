756397cae095b49482ec30e8cb8f69732f9a11f5
Arjun Dhamrait <adhamrait11@gmail.com>
2020-10-30 19:15:50 -0700

    Continuity ETFStatic (#98)
    * MenuLeft: Select/add/remove stuntsheets
    
    * Add and edit continuities
    
    * Added ETFStatic continuity
    
    * Added facing directions to ContETFStatic
    
    * Removed ui option for editing facing direction
    
    * Added unit tests for ContETFStatic
    
    * linted
    
    * Added support for diagonal marching
    
    * Removed unused facingDirection code
    
    * Linted again
    
    * Added check for defaulting facingDirection
    
    * linted
    
    Co-authored-by: Michael Tamaki <themichaeltamaki@gmail.com>

--- src/components/menu-right/DotTypeEditor.vue
+++ src/components/menu-right/DotTypeEditor.vue
@@ -30,6 +30,11 @@
           data-test="menu-right--add-etf-dynamic"
           >Eight to Five Dynamic</b-dropdown-item
         >
+        <b-dropdown-item
+          @click="addContETFStatic"
+          data-test="menu-right--add-etf-static"
+          >Eight to Five Static</b-dropdown-item
+        >
         <b-dropdown-item @click="addContEven" data-test="menu-right--add-even"
           >Even</b-dropdown-item
         >
@@ -42,6 +47,7 @@
 <script lang="ts">
 import BaseCont from "@/models/continuity/BaseCont";
 import ContETFDynamic from "@/models/continuity/ContETFDynamic";
+import ContETFStatic from "@/models/continuity/ContETFStatic";
 import ContInPlace from "@/models/continuity/ContInPlace";
 import ContEven from "@/models/continuity/ContEven";
 import StuntSheet from "@/models/StuntSheet";
@@ -80,6 +86,12 @@ export default Vue.extend({
       this.$store.commit("addContinuity", {
         dotTypeIndex: this.dotTypeIndex,
         continuity: new ContETFDynamic(),
+      });
+    },
+    addContETFStatic() {
+      this.$store.commit("addContinuity", {
+        dotTypeIndex: this.dotTypeIndex,
+        continuity: new ContETFStatic(),
       });
     },
     addContEven() {
--- src/models/continuity/ContETFStatic.ts
+++ src/models/continuity/ContETFStatic.ts
@@ -3,6 +3,7 @@ import StuntSheetDot from "../StuntSheetDot";
 import { DIRECTIONS, MARCH_TYPES } from "../util/constants";
 import { FlowBeat } from "../util/types";
 import Serializable from "../util/Serializable";
+import { startPositionHelper } from "./continuity-util";
 
 /**
  * Move in a specified direction for a duration. Can only move in an
@@ -11,9 +12,7 @@ import Serializable from "../util/Serializable";
  * - FMMM 4 SW
  *
  * @property marchingDirection - Which direction the marcher is moving
- * @property facingDirection   - Which direction the marcher is facing during
- *                               the movement. If undefined, will be
- *                               marchingDirection.
+ * @property facingDirection - Which direction the marcher is facing
  */
 export default class ContETFStatic
   extends Serializable<ContETFStatic>
@@ -40,8 +39,13 @@ export default class ContETFStatic
 
   getHumanReadableText(): string {
     if (this.humanReadableText !== "") return this.humanReadableText;
-    // TODO: Implement
-    return "";
+    const marchingDirectionText: string = DIRECTIONS[this.marchingDirection];
+    const facingDirectionText: string = DIRECTIONS[this.facingDirection];
+    if (this.marchingDirection === this.facingDirection) {
+      return `FM${this.marchType} ${this.duration} ${marchingDirectionText}`;
+    } else {
+      return `FM${this.marchType} ${this.duration} ${marchingDirectionText} FACING ${facingDirectionText}`;
+    }
   }
 
   /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -50,7 +54,23 @@ export default class ContETFStatic
     startDot: StuntSheetDot,
     endDot?: StuntSheetDot
   ): void {
-    // TODO: Implement
+    const dx = Math.sign(
+      Math.round(Math.cos((this.marchingDirection / 180) * Math.PI))
+    );
+    const dy = Math.sign(
+      Math.round(Math.sin((this.marchingDirection / 180) * Math.PI))
+    );
+
+    const [x, y]: [number, number] = startPositionHelper(flow, startDot);
+    for (let beat = 1; beat <= this.duration; beat += 1) {
+      const flowBeat: FlowBeat = {
+        x: x + dx * (beat - 1),
+        y: y + dy * (beat - 1),
+        direction: this.facingDirection,
+        marchType: this.marchType,
+      };
+      flow.push(flowBeat);
+    }
   }
   /* eslint-enable @typescript-eslint/no-unused-vars */
 }
