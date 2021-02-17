547bc5266853587c8bbcec33ec0ddec721739c38
Richard Powell <rmpowell77@me.com>
2021-02-16 21:22:18 -0800

    Fixing up the broken lasso.  Adding unit tests to catch in the future (#152)

--- tests/e2e/specs/tools/ToolBoxSelect.spec.js
+++ tests/e2e/specs/tools/ToolBoxSelect.spec.js
@@ -45,4 +45,29 @@ describe("tools/ToolBoxSelect", () => {
       });
     });
   });
+  it("make sure selection works", () => {
+    cy.visit("/").get('[data-test="menu-bottom-tool--add-rm"]').click();
+    cy.mousedownGrapher(12, 8).mouseupGrapher(12, 8);
+    // should not have selection
+    cy.get('[data-test="grapher-dots--dot"]').should("have.length", 1);
+    cy.get('[data-test="grapher-dots--dot"][data-test-selected="true"]').should(
+      "have.length",
+      0
+    );
+    cy.get('[data-test="menu-bottom-tool--select-box-move"]').click();
+    cy.get('[data-test="grapher-field--rect"]').then(() => {
+      cy.mousedownGrapher(24, 2);
+      cy.mousemoveGrapher(10, 10);
+      cy.mouseupGrapher(24, 2);
+
+      cy.get('[data-test="grapher-tool--selection-lasso"]').then((polyline) => {
+        cy.wrap(polyline).should("have.attr", "points");
+      });
+    });
+    // should have selection
+    cy.get('[data-test="grapher-dots--dot"][data-test-selected="true"]').should(
+      "have.length",
+      1
+    );
+  });
 });
